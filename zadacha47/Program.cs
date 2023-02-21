06355a343b9a40c7f6bac650cd5d7b9bf6793125
“Ivan <ivkriv@yandex.ru>
2023-02-21 11:32:29 +0300

    resheno47

--- zadacha47/Program.cs
+++ zadacha47/Program.cs
@@ -1,19 +1,38 @@
-﻿Console.WriteLine("Задайте количество строк двумерного массива:");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Задайте количество столбцов двумерного массива:");
-int n = Convert.ToInt32(Console.ReadLine());
-double[,] twoDimArray = new double[m, n];
-Random rnd = new Random();
-void PrintArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { Console.Write($"{matr[i, j]} ");}
- Console.WriteLine();}}
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
 
-void FillArray(double[,] matr)
-{ for (int i = 0; i < m; i++)
- { for (int j = 0; j < n; j++)
- { matr[i,j] = Convert.ToDouble(rnd.Next(-100, 100)/10.0);}}}
-FillArray(twoDimArray);
-Console.WriteLine();
-PrintArray(twoDimArray);
+Console.WriteLine("Введите количество строк  ");
+int linesVol = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columnsVol = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.Write("]");
+        Console.WriteLine("");
+    }
+}
